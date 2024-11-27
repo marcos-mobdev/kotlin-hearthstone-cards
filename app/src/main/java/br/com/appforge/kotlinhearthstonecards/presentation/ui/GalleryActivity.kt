0b2488a09dc2367a307e0c50a4f20ead3bc300cf
Marcos Pinheiro <marcospinheiro.em@gmail.com>
2024-11-27 20:55:07 -0300

    4 - Added Gallery Activity

--- app/src/main/java/br/com/appforge/kotlinhearthstonecards/presentation/ui/GalleryActivity.kt
+++ app/src/main/java/br/com/appforge/kotlinhearthstonecards/presentation/ui/GalleryActivity.kt
@@ -1,16 +1,17 @@
-package br.com.appforge.kotlinhearthstonecards
+package br.com.appforge.kotlinhearthstonecards.presentation.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import br.com.appforge.kotlinhearthstonecards.R
 
-class MainActivity : AppCompatActivity() {
+class GalleryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_gallery)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
