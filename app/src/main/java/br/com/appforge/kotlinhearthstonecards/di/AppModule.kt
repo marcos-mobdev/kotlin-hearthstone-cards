37e2157edc96e9755160fd88557846c09ebd8421
Marcos Pinheiro <marcospinheiro.em@gmail.com>
2024-11-27 20:48:08 -0300

    2 - Depedency Injection Setup
    Added BaseApplication for hilt injection, AppModule to provide instances, and constants object

---
+++ app/src/main/java/br/com/appforge/kotlinhearthstonecards/di/AppModule.kt
@@ -0,0 +1,12 @@
+package br.com.appforge.kotlinhearthstonecards.di
+
+import androidx.lifecycle.ViewModel
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+
+@Module
+@InstallIn(ViewModelComponent::class)
+object AppModule {
+
+}
